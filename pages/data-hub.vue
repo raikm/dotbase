6cc71aa1fcf58234d7bd756fd7105619f80c3b9d
Raik <raik.mueller@auros-consulting.de>
2025-03-08 13:29:25 +0100

    feat(ui): wip chartjs with placeholder

--- pages/data-hub.vue
+++ pages/data-hub.vue
@@ -9,19 +9,80 @@
         <Column field="status" header="Status" />
       </DataTable>
       <div class="flex-1/3">
-        <!-- Diagram here -->
+        <Chart type="radar" :data="chartData" :options="chartOptions" class="w-full md:w-[30rem]" />
       </div>
     </div>
     <div v-else>
-      <!-- Loading or no quest. -->
+      <!-- no questionnairs found -->
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Questionnaire } from '~/types/Questionnaire'
+import Chart from 'primevue/chart';
+
 const questionnaires = ref<Questionnaire[]>()
 const { data } = await useFetch<Questionnaire[]>(`/api/questionnaires`)
 questionnaires.value = data.value || []
 
+onMounted(() => {
+  chartData.value = setChartData();
+  chartOptions.value = setChartOptions();
+})
+
+
+const chartData = ref();
+const chartOptions = ref();
+        
+const setChartData = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--p-text-color');
+
+    return {
+        labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                borderColor: documentStyle.getPropertyValue('--p-gray-400'),
+                pointBackgroundColor: documentStyle.getPropertyValue('--p-gray-400'),
+                pointBorderColor: documentStyle.getPropertyValue('--p-gray-400'),
+                pointHoverBackgroundColor: textColor,
+                pointHoverBorderColor: documentStyle.getPropertyValue('--p-gray-400'),
+                data: [65, 59, 90, 81, 56, 55, 40]
+            },
+            {
+                label: 'My Second dataset',
+                borderColor: documentStyle.getPropertyValue('--p-pink-400'),
+                pointBackgroundColor: documentStyle.getPropertyValue('--p-pink-400'),
+                pointBorderColor: documentStyle.getPropertyValue('--p-pink-400'),
+                pointHoverBackgroundColor: textColor,
+                pointHoverBorderColor: documentStyle.getPropertyValue('--p-pink-400'),
+                data: [28, 48, 40, 19, 96, 27, 100]
+            }
+        ]
+    };
+};
+const setChartOptions = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--p-text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+
+    return {
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor
+                }
+            }
+        },
+        scales: {
+            r: {
+                grid: {
+                    color: textColorSecondary
+                }
+            }
+        }
+    };
+}
 </script>
